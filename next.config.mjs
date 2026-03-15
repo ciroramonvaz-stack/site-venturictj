86fea80e4a44feb34950f2c364018aa360f8bdc2
ciroramonvaz-stack <ciroramonvaz@gmail.com>
2026-03-15 10:12:01 -0300

    fix: add basePath and assetPrefix for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+    basePath: '/site-venturictj',
+    assetPrefix: '/site-venturictj/',
   images: {
     unoptimized: true
   },
