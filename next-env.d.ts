0a7428d1bc7ccdc07f9cba3c6158809daee8c122
ciroramonvaz-stack <ciroramonvaz@gmail.com>
2026-03-30 23:25:29 -0300

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,10 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
+diff --git a/next-env.d.ts b/next-env.d.ts
+index 84ab714bdef68903103f153806cfe168ecbb94f8..40c3d68096c270ef976f3db4e9eb42b05c7067bb 100644
+--- a/next-env.d.ts
++++ b/next-env.d.ts
+@@ -1,4 +1,5 @@
+ /// <reference types="next" />
+ /// <reference types="next/image-types/global" />
+ 
+ // NOTE: This file should not be edited
++// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
