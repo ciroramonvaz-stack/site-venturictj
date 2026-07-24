3c54e55b868db07ffee7824ba7fafa087d4489c9
ciroramonvaz-stack <ciroramonvaz@gmail.com>
2026-07-23 22:52:24 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -248,7 +248,7 @@ export default function HomePage() {
                     'Avaliamos elegibilidade, estruturamos projetos e capturamos receita ambiental do seu ativo florestal regularizado.',
                   icon: (
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 0-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
                     </svg>
                   ),
                 },
@@ -335,7 +335,7 @@ export default function HomePage() {
 
             <div className="flex flex-wrap justify-center lg:justify-start gap-12 mb-12">
               {[
-                { name: 'Ciro Ramon Vaz', role: 'Fundador & Operações', credential: 'Bacharel em Direito', image: '/ciro.jpg.jpeg' },
+                { name: 'Ciro Ramon Vaz', role: 'Fundador & Operações', credential: 'OAB/PR 136.171', image: '/ciro.jpg.jpeg' },
                 { name: 'Eduardo Gabriel', role: 'Diretor Jurídico', credential: 'OAB/PR 58.941', image: '/eduardo.jpg.jpeg' },
                 { name: 'Geliandra Lopes', role: 'Direito Ambiental', credential: 'OAB/PR 115.178', image: '/geliandra.jpg.jpeg' },
                 { name: 'José Alaertes', role: 'Diretor Empresarial', credential: 'OAB/PR 60.934', image: '/jose.jpg.jpeg' },
